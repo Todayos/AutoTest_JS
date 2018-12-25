c7733136021cd72bedd439aefd186ca8a97d8364
wangzd <734327367@qq.com>
2018-12-25 17:00:42 +0800

    修改jquery

--- test/test.js
+++ test/test.js
@@ -6,10 +6,3 @@ $(document).ready(function () {
         $(".bg2").show();
     });
 });
-
-var arr;
-if (arr.length==1){
-    return sum=arr[0];
-}else if(arr.length==2){
-    return sum=arr[0]+arr[0];
-}
