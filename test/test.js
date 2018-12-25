841fb9e6a0036929615696597fa21cac577f11ce
wangzd <734327367@qq.com>
2018-12-25 17:00:15 +0800

    新增了jquery文件

---
+++ test/test.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".h1").click(function () {
+        $(".bg1").hide();
+    });
+    $(".h1").click(function () {
+        $(".bg2").show();
+    });
+});
+
+var arr;
+if (arr.length==1){
+    return sum=arr[0];
+}else if(arr.length==2){
+    return sum=arr[0]+arr[0];
+}
